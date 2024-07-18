353481cfa7bcf782762a363e85b7a66ec7b73e42
Mateus Ceci <mateusceci@gmail.com>
2024-07-18 16:43:23 -0300

    create empty motivation, about and hobbies page

---
+++ components/Hobbies.tsx
@@ -0,0 +1,9 @@
+
+
+const Hobbies = () => {
+  return (
+    <div className="h-screen" id="hobbies">Hobbies</div>
+  )
+}
+
+export default Hobbies
---
+++ components/Motivation.tsx
@@ -0,0 +1,9 @@
+
+
+const Motivation = () => {
+  return (
+    <div className="h-screen" id="motivation">Motivation</div>
+  )
+}
+
+export default Motivation
