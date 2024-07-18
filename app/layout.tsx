7bd428857a6dcace6ec38200aa12918cbd151682
Mateus Ceci <mateusceci@gmail.com>
2024-07-18 16:42:28 -0300

    adjust layout to have only children

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Sidebar from "@/components/Sidebar";
+import Topnav from "@/components/Topnav";
 
 const inter = Inter({ subsets: ["latin"] });
 
