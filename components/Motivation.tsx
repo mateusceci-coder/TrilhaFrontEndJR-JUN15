088cb44a1646b151da5e5122f5e2cb0b910ae4d2
Mateus Ceci <mateusceci@gmail.com>
2024-07-18 20:24:57 -0300

    add motion effect and use effect to handle scroll

--- components/Motivation.tsx
+++ components/Motivation.tsx
@@ -1,25 +1,72 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+
 const Motivation = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const content = document.getElementById("motivation");
+      if (content) {
+        const rect = content.getBoundingClientRect();
+        if (rect.top < window.innerHeight / 1.5) {
+          setIsVisible(true);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    handleScroll();
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="h-screen flex flex-col items-center p-4" id="motivation">
-      <h1 className="font-kanit text-5xl mb-24 text-red-primary">Motivação</h1>
-      <div className="flex flex-col text-center max-w-4xl">
-      <p className="text-xl mb-4 leading-9">
-        Minha motivação principal para ingressar na Código Certo é a
-        oportunidade de voltar a trabalhar em equipe. Acredito firmemente que a
-        melhor forma de evoluir no campo da programação é enfrentar problemas
-        reais, colaborar com outras pessoas e sair constantemente da zona de
-        conforto. Durante dois meses, tive a valiosa experiência de atuar como
-        freelancer para uma empresa americana, período que considero um dos mais
-        enriquecedores da minha carreira.
-      </p>
-      <p className="text-xl leading-9">
-        Estou determinado a ampliar meus conhecimentos e contribuir ativamente
-        em qualquer tipo de projeto. Meu objetivo é não apenas aprender mais,
-        mas também agregar valor significativo à equipe e aos projetos da Código
-        Certo.
-      </p>
-
-      </div>
+      {isVisible && (
+        <>
+          <motion.h1
+            className="font-kanit text-5xl mb-24 text-red-primary"
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            Motivação
+          </motion.h1>
+          <div className="flex flex-col text-center max-w-4xl">
+            <motion.p
+              className="text-xl mb-4 leading-9"
+              initial={{ opacity: 0, x: -100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5, delay: 0.5 }}
+            >
+              Minha motivação principal para ingressar na Código Certo é a
+              oportunidade de voltar a trabalhar em equipe. Acredito firmemente
+              que a melhor forma de evoluir no campo da programação é enfrentar
+              problemas reais, colaborar com outras pessoas e sair
+              constantemente da zona de conforto. Durante dois meses, tive a
+              valiosa experiência de atuar como freelancer para uma empresa
+              americana, período que considero um dos mais enriquecedores da
+              minha carreira.
+            </motion.p>
+            <motion.p
+              className="text-xl leading-9"
+              initial={{ opacity: 0, x: 100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5, delay: 1 }}
+            >
+              Estou determinado a ampliar meus conhecimentos e contribuir
+              ativamente em qualquer tipo de projeto. Meu objetivo é não apenas
+              aprender mais, mas também agregar valor significativo à equipe e
+              aos projetos da Código Certo.
+            </motion.p>
+          </div>
+        </>
+      )}
     </div>
   );
 };
