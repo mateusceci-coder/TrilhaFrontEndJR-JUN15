827f6ed88e3be8f2334b886b2cf0a2967ada7ed2
Mateus Ceci <mateusceci@gmail.com>
2024-07-18 20:14:40 -0300

    create motivation page

--- components/Motivation.tsx
+++ components/Motivation.tsx
@@ -1,9 +1,27 @@
-
-
 const Motivation = () => {
   return (
-    <div className="h-screen" id="motivation">Motivation</div>
-  )
-}
+    <div className="h-screen flex flex-col items-center p-4" id="motivation">
+      <h1 className="font-kanit text-5xl mb-24">Motivação</h1>
+      <div className="flex flex-col text-center max-w-4xl">
+      <p className="text-xl mb-4 leading-9">
+        Minha motivação principal para ingressar na Código Certo é a
+        oportunidade de voltar a trabalhar em equipe. Acredito firmemente que a
+        melhor forma de evoluir no campo da programação é enfrentar problemas
+        reais, colaborar com outras pessoas e sair constantemente da zona de
+        conforto. Durante dois meses, tive a valiosa experiência de atuar como
+        freelancer para uma empresa americana, período que considero um dos mais
+        enriquecedores da minha carreira.
+      </p>
+      <p className="text-xl leading-9">
+        Estou determinado a ampliar meus conhecimentos e contribuir ativamente
+        em qualquer tipo de projeto. Meu objetivo é não apenas aprender mais,
+        mas também agregar valor significativo à equipe e aos projetos da Código
+        Certo.
+      </p>
 
-export default Motivation
+      </div>
+    </div>
+  );
+};
+
+export default Motivation;
